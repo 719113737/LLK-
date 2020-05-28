f37c711bd2bccc271ec1704e8f405cff8debb0fe
Qbz0920 <65889519+Qbz0920@users.noreply.github.com>
2020-05-28 20:48:00 +0800

    Create mainwindow.cpp

---
+++ mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    initial();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::gotoSecondwindow(bool)
+{
+    this->hide();
+    mWindow2->startgame();
+    mWindow2->show();
+}
+
+void MainWindow::backtoFirstwindow()
+{
+    this->show();
+}
+
+void MainWindow::initial()
+{
+    mWindow2 = new MainWindow2();
+    mWindow2->setWindowTitle("连连看");
+    connect(ui->pushButton,SIGNAL(clicked(bool)),this,SLOT(gotoSecondwindow(bool)));
+    connect(mWindow2, SIGNAL(gotoFirstWindow1()), this, SLOT(backtoFirstwindow()));
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    this->hide();
+    mWindow2->startgame();
+    mWindow2->show();
+}
