7b59f06474c393bdc7d1039e96c46d392de86de4
Qbz0920 <65889519+Qbz0920@users.noreply.github.com>
2020-06-05 09:32:45 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(showDialog()));
-    connect(ui->pushButtonRecord,SIGNAL(clicked()),this,SLOT(Record()));
-
+    initial();
 }
 
 MainWindow::~MainWindow()
@@ -16,3 +14,40 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::gotoSecondwindow(bool)
+{
+    this->hide();
+    mWindow2->hard();
+    mWindow2->startgame();
+    mWindow2->show();
+}
+
+void MainWindow::backtoFirstwindow()
+{
+    this->show();
+}
+
+void MainWindow::initial()
+{
+    mWindow2 = new MainWindow2();
+    mWindow2->setWindowTitle("连连看");
+    connect(ui->pushButton,SIGNAL(clicked(bool)),this,SLOT(gotoSecondwindow(bool)));
+    connect(mWindow2, SIGNAL(gotoFirstWindow1()), this, SLOT(backtoFirstwindow()));
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    this->hide();
+    mWindow2->easy();
+    mWindow2->startgame();
+    mWindow2->show();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    this->hide();
+    mWindow2->normal();
+    mWindow2->startgame();
+    mWindow2->show();
+}
